660f89fe70992492a79fc35b832e04420846c141
gabri <gabriellisilva1400@gmail.com>
2024-09-23 14:57:23 -0300

    FEAT: Logger Unit Test.

--- AstarMaze.Tests/UnitTests/LoggerTests.cs
+++ AstarMaze.Tests/UnitTests/LoggerTests.cs
@@ -1,1 +1,41 @@
-﻿
+﻿using System.IO;
+using Xunit;
+using AstarMaze.App.Infrastructure.Loggers;
+
+namespace AstarMaze.Tests.Infrastructure.Loggers
+{
+    public class LogServiceTests
+    {
+        [Fact]
+        public void LogService_ShouldCreateFile_WithCorrectHeaders()
+        {
+            string fileName = "testLog";
+            string filePath = $"{fileName}.csv";
+
+            var logService = new LogService(fileName);
+
+            Assert.True(File.Exists(filePath)); 
+            var fileContents = File.ReadAllLines(filePath);
+            Assert.Equal("Comando,SensorEsquerdo,SensorDireito,SensorFrontal,Carga", fileContents[0]);
+
+            
+            File.Delete(filePath); 
+        }
+
+        [Fact]
+        public void RegisterLog_ShouldAppendToFile_WithCorrectData()
+        {
+            string fileName = "testLog";
+            string filePath = $"{fileName}.csv";
+            var logService = new LogService(fileName);
+
+            logService.RegisterLog("LIGAR", "VAZIO", "VAZIO", "PAREDE", "SEM CARGA");
+
+            var fileContents = File.ReadAllLines(filePath);
+            Assert.Equal("Comando,SensorEsquerdo,SensorDireito,SensorFrontal,Carga", fileContents[0]); 
+            Assert.Equal("LIGAR,VAZIO,VAZIO,PAREDE,SEM CARGA", fileContents[1]); 
+
+            File.Delete(filePath); 
+        }
+    }
+}
